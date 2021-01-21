4528f88a85f5f849e94de4db240832c6e3295dd8
jagdeepsgill888 <59809683+jagdeepsgill888@users.noreply.github.com>
2021-01-20 19:42:23 -0500

    wk2 vid1 hardcode homepage setup

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-This is a Movie CMS Project
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to the Movies CMS</title>
+</head>
+<body>
+    <header>
+    <h2>This content could be your nav</h2>
+    <ul>
+   <li><a href="#">Action</a></li>
+   <li><a href="#">Comedy</a></li>
+   <li><a href="#">Fmaily</a></li>
+   <li><a href="#">All</a></li>
+   
+    </ul>
+    </header>
+
+    <div class="movie-item">
+    <img src="images/gravity.jpg" alt="gravity cover Image">
+    <h2>Gravity</h2>
+    <h4>Movie Released: 2013</h4>
+    <a href="#">More details...</a>
+    </div>
+
+
+<footer>
+<p>Copyright &#xA9; 2021</p>
+</footer>
+
+</body>
+</html>
