ac48dfa445fbf936dcf7f11ee0569ca64f782c40
jagdeepsgill888 <59809683+jagdeepsgill888@users.noreply.github.com>
2021-01-25 22:27:19 -0500

    video3 almost half

--- admin/scripts/read.php
+++ admin/scripts/read.php
@@ -13,3 +13,18 @@ function getAllMovies()
         return 'There was a problem accessing this info';
     }
 }
+
+function getSingleMovie($id)
+{
+    $pdo = Database::getInstance()->getConnection();
+    ## TODO: finish the line with a proper SQL query that only fetch movie for the given id
+    $querySingle = '';
+    $runSingle = $pdo->query($querySingle);
+    $movie = $runSingle->fetch(PDO::FETCH_ASSOC);
+
+    if ($movie) {
+        return $movie;
+    } else {
+        return 'There was a problem to fetch single movie for'.$id;
+    }
+}
---
+++ details.php
@@ -0,0 +1,50 @@
+<?php
+require_once './config/database.php';
+require_once './admin/scripts/read.php';
+
+
+  $getMovies = getAllMovies();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <header>
+    <h2>This content could be your nav</h2>
+    <ul>
+   <li><a href="#">Action</a></li>
+   <li><a href="#">Comedy</a></li>
+   <li><a href="#">Fmaily</a></li>
+   <li><a href="#">All</a></li>
+   
+    </ul>
+    </header>
+
+ <?php foreach ($getMovies as $movie):?>
+    <div class="movie-item">
+    <img src="images/<?php echo $movie['movies_cover']; ?>" alt="<?php echo $movie['movies_title']; ?> cover Image">
+
+    <h2><?php echo $movie['movies_title']; ?></h2>
+
+    <h4>Movie Released: <?php echo $movie['movies_release']; ?></h4>
+    <p><?php echo $movie['movies_storyline']; ?></p>
+    <a href="#">More details...</a>
+    </div>
+    <?php endforeach;?>
+
+    
+<footer>
+<p>Copyright &#xA9; <?php echo date('Y');?>
+</p>
+</footer>
+
+</body>
+</html>
