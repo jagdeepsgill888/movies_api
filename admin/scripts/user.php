0ced9515356ff0228a656eff9e00819e3efc3ea6
jagdeepsgill888 <59809683+jagdeepsgill888@users.noreply.github.com>
2021-02-15 19:18:28 -0500

    end of vid2 created user & fix SQL injection

--- admin/scripts/user.php
+++ admin/scripts/user.php
@@ -3,5 +3,31 @@
 function createUser($user_data)
 {
     ## Testing only, remove it later
-    return var_export($user_data, true);
+    // return var_export($user_data, true);
+
+    ## 1.Run the proper SQL query to start to insert user
+    $pdo = Database::getInstance()->getConnection();
+
+    $create_user_query = 'INSERT INTO tbl_user(user_fname, user_name, user_pass, user_email)';
+    $create_user_query .= ' VALUES(:fname, :username, :password, :email)';
+    
+    
+    $create_user_set = $pdo->prepare($create_user_query);
+    $create_user_result = $create_user_set->execute(
+        array(
+                ':fname'=>$user_data['fname'],
+                ':username'=>$user_data['username'],
+                ':password'=>$user_data['password'],
+                ':email'=>$user_data['email'],
+        )
+    );
+
+    ## 2. Redirect to index.php if created user successfully, (maybe with some message??)
+    # otherwise, show the error message
+
+    if ($create_user_result) {
+        redirect_to('index.php');
+    } else {
+        return 'The user did not go through!!';
+    }
 }
